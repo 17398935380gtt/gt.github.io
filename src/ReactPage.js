293ba1c791b2fd5b09a08ab7fde721aa867796b3
高婷婷 <gaotingting@gaotingtingdeMacBook-Air.local>
2020-03-23 02:00:10 +0800

    update

---
+++ src/ReactPage.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import React from 'react';
+import ReactDom from 'react-dom';
+import "./ReactPage.less";
+import TestImg from "./img/WechatIMG1319.jpeg";
+import ES6Test from "./ES6Demo/test_promise";
+
+ class ReactPage extends React.Component{
+     render() {
+         return <div id="ReactText">react text 测试watch 热更新测试
+            <img src={TestImg} style={{width:"auto",height:"auto"}} alt=""/>
+         </div>
+     }
+ }
+
+ReactDom.render(
+    <ES6Test/>,document.getElementById('root')
+);
+
+ //npm run build 打包之后，在dist文件夹里通过html文件调用（要添加root节点）
